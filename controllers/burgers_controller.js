7461f7eddfd1e87c346f43471397ae2eed430451
John Garner <jdextergarneriv@gmail.com>
2020-12-01 15:41:46 -0500

    completed get post and put routes in burgers_controller

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var router = express.Router();
+
+var burger = require("../models/burger.js");
+
+router.get("/", function (req, res) {
+	burger.selectAll(function (data) {
+		res.render("index", { burgers: data });
+	});
+});
+
+router.post("/api/burgers", function (req, res) {
+	burger.insertOne(["burger_name"], [req.body.burger_name], function (result) {
+		res.json({ id: result.insertID });
+	});
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+	burger.updateOne(["devoured"], [1], [req.params.id], function (result) {
+		if (result.changedRows == 0) {
+			return res.status(404).end();
+		} else {
+			res.status(200).end();
+		}
+	});
+});
