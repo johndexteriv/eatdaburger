e0b905ac9b34ae2bd757bf4fe0804d66ed06ae0b
John Garner <jdextergarneriv@gmail.com>
2020-12-01 16:03:58 -0500

    developed server.js file setting handlebars as view enginge and gigivng server access to routes

--- server.js
+++ server.js
@@ -4,3 +4,19 @@ var PORT = process.env.PORT || 8080;
 var app = express();
 
 app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main " }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function () {
+	console.log("Server Listening on: http://localhost:" + PORT);
+});
