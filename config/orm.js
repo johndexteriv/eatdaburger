02269eb63cdba9a4b05e5fe331cb177894fb1530
John Garner <jdextergarneriv@gmail.com>
2020-12-01 14:44:17 -0500

    completed orm selectAll, insertOne and updateOne functions

---
+++ config/orm.js
@@ -0,0 +1,41 @@
+var connection = require("../config/connection.js");
+
+var orm = {
+	selectAll: function (tableInput, cb) {
+		var queryString = "SELECT * FROM ??";
+		connection.query(queryString, [tableInput], function (err, result) {
+			if (err) {
+				throw err;
+			}
+			cb(result);
+		});
+	},
+	insertOne: function (tableInput, colInput, values, cb) {
+		var queryString = "INSERT INTO ?? (??) VALUES (?)";
+		connection.query(
+			queryString,
+			[tableInput, colInput, values],
+			function (err, result) {
+				if (err) {
+					throw err;
+				}
+				cb(result);
+			}
+		);
+	},
+	updateOne: function (tableInput, colToUpdate, valToUpdate, id, cb) {
+		var queryString = "UPDATE ?? SET ?? = ? WHERE id = ?";
+		connection.query(
+			queryString,
+			[tableInput, colToUpdate, valToUpdate, id],
+			function (err, result) {
+				if (err) {
+					throw err;
+				}
+				cb(result);
+			}
+		);
+	},
+};
+
+module.exports = orm;
