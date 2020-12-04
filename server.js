4e0feeb07e840f957111a7630af08b68aceec786
Noah King <noah@horizon3.ai>
2020-12-04 17:32:50 -0500

    old scout style

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
+
 const scout = require('@scout_apm/scout-apm');
-const process = require('process');
 const express = require('express');
 
-var PORT = process.env.PORT || 8080;
+async function start() {
+  // Trigger the download and installation of the core-agent
+  await scout.install({
+    allowShutdown: false, // allow shutting down spawned scout-agent processes from this program
+    monitor: true, // enable monitoring
+    name: 'eat da burger',
+    key: process.env.SCOUT_KEY,
+  });
+}
 
-// Initialize the express application
-const app = express();
+var PORT = process.env.PORT || 8080;
+var app = express();
 
 // Enable the app-wide scout middleware
 app.use(scout.expressMiddleware());
@@ -24,31 +32,8 @@ var routes = require('./controllers/burgers_controller.js');
 
 app.use(routes);
 
-// Shut down the core-agent when this program exits
-process.on('exit', () => {
-  if (app && app.scout) {
-    app.scout.shutdown();
-  }
+app.listen(PORT, function () {
+  console.log('Server Listening on: http://localhost:' + PORT);
 });
 
-// Start application
-async function start() {
-  // Install and wait for scout to set up
-  await scout.install({
-    monitor: true, // enable monitoring
-    name: 'eat da burger',
-    key: 'zT6DaR4cWVqT2DdZzUm3',
-
-    // allow scout to be shutdown when the process exits
-    allowShutdown: true,
-  });
-
-  // Start the server
-  app.listen(PORT, function () {
-    console.log('Server Listening on: http://localhost:' + PORT);
-  });
-}
-
-if (require.main === module) {
-  start();
-}
+start();
