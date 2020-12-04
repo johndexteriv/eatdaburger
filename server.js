b5816b9910b3c5a5c314ee3abafac297ed1513a7
Noah King <noah@horizon3.ai>
2020-12-04 17:01:20 -0500

    scout

--- server.js
+++ server.js
@@ -1,22 +1,38 @@
-const express = require("express");
+const scout = require('@scout_apm/scout-apm');
+const express = require('express');
+
+async function start() {
+  // Trigger the download and installation of the core-agent
+  await scout.install({
+    allowShutdown: false, // allow shutting down spawned scout-agent processes from this program
+    monitor: true, // enable monitoring
+    name: 'eat da burger',
+    key: process.env.SCOUT_KEY,
+  });
+}
 
 var PORT = process.env.PORT || 8080;
 var app = express();
 
-app.use(express.static("public"));
+// Enable the app-wide scout middleware
+app.use(scout.expressMiddleware());
+
+app.use(express.static('public'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var exphbs = require("express-handlebars");
+var exphbs = require('express-handlebars');
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
-var routes = require("./controllers/burgers_controller.js");
+var routes = require('./controllers/burgers_controller.js');
 
 app.use(routes);
 
 app.listen(PORT, function () {
-	console.log("Server Listening on: http://localhost:" + PORT);
+  console.log('Server Listening on: http://localhost:' + PORT);
 });
+
+start();
